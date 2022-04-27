20d8585910ebbb8205ab63301641e477c2e3c11d
hyuzni <75052835+hyuzni@users.noreply.github.com>
2022-04-27 10:39:59 +0900

    update rest parameters
    update rest parameters

--- es6/parameters.js
+++ es6/parameters.js
@@ -1,5 +1,4 @@
 // default parameters
-
 function sum(value, size=1){
     return value * size;
 }
@@ -11,3 +10,27 @@ function sum1(value, size={value:1}){
 }
 console.log(sum1(3,{value:5})); // 15
 console.log(sum1(3)); // 3
+
+// rest parameters
+function checkNum(){ 
+    const argArray = Array.prototype.slice.call(arguments); // arguments의 개체들을 슬라이스 해서 array prototype에 담는다
+
+    console.log(toString.call(argArray)); // object Array
+    console.log(toString.call(arguments)) // object arguments
+    console.log(argArray); // [10,2,3,4,5,"55"] 
+    
+    const result = argArray.every((v) => typeof v === "number"); // every : 모든 값이 true 인지 체크.
+    console.log(result); // false ("55"는 number type 이 아님.)
+}
+
+// rest parameters es6
+function checkNum2(...argArray){ // Array.prototype.slice.call(arguments) 와 같은 값. 진짜 배열
+
+    console.log(toString.call(argArray)); // object Array
+    console.log(argArray); // [10,2,3,4,5] 
+    
+    const result = argArray.every((v) => typeof v === "number");
+    console.log(result);
+}
+const result = checkNum(10,2,3,4,5,"55");
+const result2 = checkNum2(10,2,3,4,5);
