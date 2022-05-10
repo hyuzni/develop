658ae0761819476ca03659cdae8911b121d8bf41
hyuzni <75052835+hyuzni@users.noreply.github.com>
2022-05-10 17:51:39 +0900

    upload controller

--- lecture-vue/vanilla/js/controllers/MainController.js
+++ lecture-vue/vanilla/js/controllers/MainController.js
@@ -1,14 +1,39 @@
-import FormView from "../views/FormView.js"
-const tag = "[MainController]"
+import FormView from "../views/FormView.js";
+import ResultView from "../views/ResultView.js"
+
+import SearchModel from "../models/SearchModel.js"
+
+const tag = "[MainController]";
 
 export default {
     init() {
-        console.log(tag, "init()")
+        console.log(tag, "init()");
         FormView.setup(document.querySelector("form"))
-                .on("@submit", e => this.onSubmit(e.detail.input))
+                .on("@submit", e => this.onSubmit(e.detail.input)) // {input : this.inputEl.value}
+                .on("@reset", () => this.onResetForm()); // event 수신기 부착
+        
+        ResultView.setup(document.querySelector("#search-result"));
+    },
+
+    search (query) {
+        console.log(tag, "search()", query)
+        SearchModel.list(query).then(data => {
+            this.onSearchResult(data)
+        })
+        // search api
+        this.onSearchResult([])
     },
 
     onSubmit(input) {
-        console.log(tag, "onSubmit()", input)
+        console.log(tag, "onSubmit()", input);
+        this.search(input)
+    },
+
+    onResetForm() {
+        console.log(tag, "onResetForm()");
+    },
+
+    onSearchResult(data) {
+        ResultView.render(data)
     }
-}
+};
