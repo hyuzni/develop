d5e1e6cc4cbe178b880e7540b2a853e878f05651
hyuzni <75052835+hyuzni@users.noreply.github.com>
2022-05-10 17:50:41 +0900

    view upload
    view upload

--- lecture-vue/vanilla/js/views/FormView.js
+++ lecture-vue/vanilla/js/views/FormView.js
@@ -1,32 +1,38 @@
-import View from "./View.js"
+import View from "./View.js";
 
-const tag = "[Formview]"
+const tag = "[Formview]";
 
-const FormView = Object.create(View)
+const FormView = Object.create(View); // view 를 prototype 으로 객체를 생성
 
 FormView.setup = function (el) {
-    this.init(el)
-    this.inputEl = el.querySelector("[type=text]")
-    this.resetEl = el.querySelector("[type=reset]")
-    this.showResetBtn(false)
-    this.bindEvents()
-    return this
-}
+    this.init(el); // form 개체 property 설정
+    this.inputEl = el.querySelector("[type=text]");
+    this.resetEl = el.querySelector("[type=reset]");
+    this.showResetBtn(false);
+    this.bindEvents();
+    return this;
+};
 
 FormView.showResetBtn = function (show = true) {
-    this.resetEl.style.display = show ? "block" : "none"
-}
+    this.resetEl.style.display = show ? "block" : "none";
+};
 
 FormView.bindEvents = function () {
-    this.on("submit", e => e.preventDefault())
-    this.inputEl.addEventListener("keyup", e => this.onKeyup(e))
-}
+    this.on("submit", e => e.preventDefault());
+    this.inputEl.addEventListener("keyup", e => this.onKeyup(e));
+    this.resetEl.addEventListener("click", () => this.onClickReset());
+};
 
 FormView.onKeyup = function (e) {
-    const enter = 13
-    this.showResetBtn(this.inputEl.value.length)
-    if(e.keyCode !== enter) return
-    this.emit("@submit", {input : this.inputEl.value})
-}
+    const enter = 13;
+    this.showResetBtn(this.inputEl.value.length);
+    if(!this.inputEl.value.length) this.emit("@reset");
+    if(e.keyCode !== enter) return;
+    this.emit("@submit", {input : this.inputEl.value});
+};
 
-export default FormView 
+FormView.onClickReset = function() {
+    this.emit("@reset")
+    this.showResetBtn(false);
+}
+export default FormView; 
--- lecture-vue/vanilla/js/views/View.js
+++ lecture-vue/vanilla/js/views/View.js
@@ -1,31 +1,31 @@
-const tag = '[View]'
+const tag = "[View]";
 
 export default {
     init(el) {
-        if (!el) throw el
-        this.el = el
-        return this
+        if (!el) throw el; // Uncaught undefined
+        this.el = el; 
+        return this;
     },
 
     on(event, handler) {
-        this.el.addEventListener(event, handler)
-        return this
+        this.el.addEventListener(event, handler);
+        return this;
     },
 
     emit(event, data) {
-        const evt = new CustomEvent(event, { detail: data })
-        this.el.dispatchEvent(evt)
-        return this 
+        const evt = new CustomEvent(event, { detail: data }); // custom Event  생성
+        this.el.dispatchEvent(evt); // 이벤트 발송
+        return this; 
     },
 
     hide() {
-        this.el.style.display = "none"
-        return this
+        this.el.style.display = "none";
+        return this;
     },
 
     show() {
-        this.el.style.display = ""
-        return this
+        this.el.style.display = "";
+        return this;
     }
 
-}
+};
